c1ee869e87fd62566f2696f5c957ed11c9849baa
f-masanori <pmasanori0306@gmail.com>
2021-11-04 21:37:08 +0900

    土台修正

--- api/src/main.go
+++ api/src/main.go
@@ -9,17 +9,6 @@ import (
 func main() {
 	fmt.Println("hello world")
 	userController := injector.InjectUserController()
-	// if err := infra.SetupDB(); err != nil {
-	// 	log.Fatalln("failed to setup for db")
-	// 	// DB接続できない＝サーバーとして機能しないとみなすとここで落としてもいい？
-	// 	// DB接続できない場合にも、ちゃんとレスポンスしてあげる必要がある場合は、落としてはいけない
-	// }
-	// r := infra.NewRouting(infra.DB)
 	r := gin.NewRouting(userController)
-	// r := gin.Default()
-	// r.GET("/", func(c *gin.Context) {
-	// 	c.String(200, "Hello, World")
-	// })
-
 	r.Run()
 }
